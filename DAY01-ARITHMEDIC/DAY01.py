c3da3405b6266cc1a57aa887e9db3596a148a6c9
varshinivarshini73385-del <varshinivarshini73385@gmail.com>
2026-09-17 14:42:36 +0530

    ARITHMETIC OPERATOR
    ARITHMETIC OPERATOR

---
+++ DAY01-ARITHMEDIC/DAY01.py
@@ -0,0 +1,13 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+================ RESTART: C:/Users/student120/Desktop/DAY-01.txt ===============
+enter the value of a:10
+enter the value of b:5
+Addition: 15
+Subtraction: 5
+Multiplication: 50
+Division: 2.0
+Modulus: 0
+power: 100000
+floor division: 2
