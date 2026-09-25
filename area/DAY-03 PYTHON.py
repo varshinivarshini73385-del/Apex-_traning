334464721fbcc305b7c8f779e1cfcdd0ab10db92
varshinivarshini73385-del <varshinivarshini73385@gmail.com>
2026-09-25 14:07:51 +0530

    Add files via upload

---
+++ area/DAY-03 PYTHON.py
@@ -0,0 +1,8 @@
+java=float(input("Enter the value:"))
+python=float(input("Enter the value:"))
+mysql=float(input("Enter the value:"))
+print("Total:",java+python+mysql)
+print("Average:",java+python+mysql/3)
+print("Percentage:",java+python+mysql/300*100)
+print("Remaining marks:",300-java+python+mysql)
+      
